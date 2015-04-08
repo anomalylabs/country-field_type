f0179095688f3a823008789e2a3f9b6fb6fcaf49
Ryan Thompson <service@aiwebsystems.com>
2015-04-07 20:18:42 -0500

    Config

--- resources/lang/en/config.php
+++ resources/lang/en/config.php
@@ -2,6 +2,8 @@
 
 return [
     'top_options' => [
-        'label' => 'Top Options'
+        'label'        => 'Top Options',
+        'instructions' => 'Top options help minimize searching for popular choices. Enter each option\'s ISO code like <strong>US</strong> on a new line.',
+        'placeholder'  => "US\nGB"
     ]
 ];
