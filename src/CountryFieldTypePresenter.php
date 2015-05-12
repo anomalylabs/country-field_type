6a75eac0daced2c9bfc60445172ce2b4185ea2ec
Ryan Thompson <service@aiwebsystems.com>
2015-05-12 15:58:33 -0500

    translated and value presenter methods

--- src/CountryFieldTypePresenter.php
+++ src/CountryFieldTypePresenter.php
@@ -13,4 +13,41 @@ use Anomaly\Streams\Platform\Addon\FieldType\FieldTypePresenter;
 class CountryFieldTypePresenter extends FieldTypePresenter
 {
 
+    /**
+     * The decorated object.
+     * This is for IDE support.
+     *
+     * @var CountryFieldType
+     */
+    protected $object;
+
+    /**
+     * Get the value for country key.
+     *
+     * @param null $key
+     * @return null|string
+     */
+    public function value($key = null)
+    {
+        if (!$key) {
+            $key = $this->object->getValue();
+        }
+
+        return array_get($this->object->getOptions(), $key);
+    }
+
+    /**
+     * Get the translated value for country key.
+     *
+     * @param null $key
+     * @return null|string
+     */
+    public function translated($key = null)
+    {
+        if (!$key) {
+            $key = $this->object->getValue();
+        }
+
+        return trans('anomaly.field_type.country::country.' . $key);
+    }
 }
